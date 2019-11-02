eb39114de9288ec4ffa9509685c3f95f97e0fab5
Ismael Masegosa <ismaelmfb@gmail.com>
2019-11-02 20:47:34 +0100

    + Implemented test createVideogame controlled endpoint

--- boot/src/test/java/com/masegosadev/boot/integration/videogame/CreateVideoGameControllerIT.java
+++ boot/src/test/java/com/masegosadev/boot/integration/videogame/CreateVideoGameControllerIT.java
@@ -1,12 +1,9 @@
 package com.masegosadev.boot.integration.videogame;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.masegosadev.infrastructure.model.videogame.VideoGameRequest;
-import com.masegosadev.usecase.platform.CreatePlatform;
-import com.masegosadev.usecase.videogame.CreateVideogame;
+import com.masegosadev.infrastructure.rest.videogame.model.VideogameRequest;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -35,7 +32,7 @@ public class CreateVideoGameControllerIT {
     public void given_A_Request_To_Save_Videogame_When_The_Request_Is_Send_Then_Return_The_Saved_Videogame() throws Exception {
         Set<String> platformsName = new HashSet<>();
         platformsName.add("PS4");
-        VideoGameRequest videoGameRequest = new VideoGameRequest("COD", "https://cod_logo.com", platformsName);
+        VideogameRequest videoGameRequest = new VideogameRequest("COD", "https://cod_logo.com", platformsName);
 
         mockMvc.perform(post("/createVideoGame")
                 .content(objectMapper.writeValueAsString(videoGameRequest))
